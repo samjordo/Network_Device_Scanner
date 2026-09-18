4fbc55d04338265c6fcd3bcb796453e5cf509b62
Sam Jordon <jordo@vt.edu>
2026-09-18 13:17:52 -0400

    added port scanning

--- scanner.py
+++ scanner.py
@@ -1,7 +1,8 @@
 import netifaces
 import ipaddress
-import os
-from scapy.all import *
+import ctypes
+from scapy.all import arping
+import nmap
 
 #use netifaces to get the ip address and mask of the interface and then use ipaddress to get the range of the network
 addr = netifaces.ifaddresses('MediaTek Wi-Fi 6 MT7921 Wireless LAN Card')[netifaces.AF_INET][0]['addr']
@@ -9,10 +10,16 @@ mask = netifaces.ifaddresses('MediaTek Wi-Fi 6 MT7921 Wireless LAN Card')[netifa
 range = ipaddress.ip_interface(f"{addr}/{mask}").network
 
 #check priveleges for scapy
-if not (os.geteuid() == 0):
-    print("You need to run this script as root")
+if not (ctypes.windll.shell32.IsUserAnAdmin()):
+    print("You need to run this script as an administrator")
     exit(1)
 
 #send an ARP request to the range of the network and get the MAC addresses of the devices on the network
-result = arping(str(range), verbose=False)
+answered, unanswered = arping(str(range))
+targets = [received.psrc for sent, received in answered]
+for target in targets:
+    nm = nmap.PortScanner()
+    nm.scan(target, arguments='-sS -T4 -p 1-65535')
+    print(nm[target]['tcp'].keys())
+
 
